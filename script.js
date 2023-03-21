0c248d0b8dfe010b80422209eb40a8a5e7fad8cd
Abhishekshinde12 <shindeabhishek36912@gmail.com>
2023-03-21 18:56:50 +0530

    modified the files

---
+++ script.js
@@ -0,0 +1,50 @@
+
+// .querySelector() - it only acts on the first match
+// Select all the buttons
+const buttons = document.querySelectorAll(".button");
+
+// Loop through all the buttons and add an event listener to each of them
+buttons.forEach(button => {
+  button.addEventListener("click", function() {
+    button.classList.toggle('active');
+    buttonClick("click");
+  });
+});
+
+
+// function buttonClick(event) {
+//     var button = event.target;
+//     var buttonText = button.textContent;
+  
+//     if (buttonText === "ON/OFF") 
+//     { alert("The ON/OFF button was clicked!");}
+//     else if (buttonText === "Percentage") {alert("The Percentage button was clicked!");} 
+//     else if (buttonText === "Drain") {alert("The Drain button was clicked!");} 
+//     else if (buttonText === "Quality") {alert("The Quality button was clicked!");} 
+//     else if (buttonText === "Filter check") {alert("The Filter check button was clicked!");}
+// }
+
+
+function onButtonClick(button) {
+    if (button.id === "on-off-button") {
+      alert("ON/OFF button clicked");
+    } else if (button.id === "percentage-button") {
+      alert("Percentage button clicked");
+    } else if (button.id === "drain-button") {
+      alert("Drain button clicked");
+    } else if (button.id === "quality-button") {
+      alert("Quality button clicked");
+    } else if (button.id === "filter-check-button") {
+      alert("Filter Check button clicked");
+    } else if(button.id == "ph-sensor"){
+      alert("Ph sensor has been turned on")  
+    }
+  }
+
+
+
+
+
+
+
+
